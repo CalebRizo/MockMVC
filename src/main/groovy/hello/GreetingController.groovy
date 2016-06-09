1da0b8bab9aea5508aae79b4893fbe6f09281786
CalebRizo <kaleb.rizzo@gmail.com>
2016-06-09 16:36:58 -0500

    Controller with get verb

--- src/main/groovy/hello/GreetingController.groovy
+++ src/main/groovy/hello/GreetingController.groovy
@@ -3,6 +3,7 @@ package hello
 import java.util.concurrent.atomic.AtomicLong
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST
@@ -12,6 +13,12 @@ public class GreetingController {
 
     private static final String template = 'Hello, %s!'
     private final AtomicLong counter = new AtomicLong()
+
+    @RequestMapping('/greeting')
+    Greeting greeting(@RequestParam(value='name', defaultValue='World') String name) {
+        
+        new Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
 
     @RequestMapping(value = '/greeting',  method = POST)  
     Greeting greeting(@RequestBody Person person) {
