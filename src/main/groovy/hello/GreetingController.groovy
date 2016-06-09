5ef2fab706ccc750c68d1dba24c4a55337ec72b3
CalebRizo <kaleb.rizzo@gmail.com>
2016-06-09 17:36:28 -0500

    adding multipart service

--- src/main/groovy/hello/GreetingController.groovy
+++ src/main/groovy/hello/GreetingController.groovy
@@ -1,5 +1,9 @@
 package hello
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.multipart.MultipartFile
+
 import java.util.concurrent.atomic.AtomicLong
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -9,20 +13,33 @@ import org.springframework.web.bind.annotation.RestController
 import static org.springframework.web.bind.annotation.RequestMethod.POST
 
 @RestController
+@RequestMapping('/greeting')
 public class GreetingController {
 
     private static final String template = 'Hello, %s!'
     private final AtomicLong counter = new AtomicLong()
 
-    @RequestMapping('/greeting')
+    @RequestMapping
     Greeting greeting(@RequestParam(value='name', defaultValue='World') String name) {
         
         new Greeting(id: counter.incrementAndGet(), content: String.format(template, name))
     }
 
-    @RequestMapping(value = '/greeting',  method = POST)  
+    @RequestMapping(method = POST)
     Greeting greeting(@RequestBody Person person) {
         
         new Greeting(id: counter.incrementAndGet(), content: String.format(template, person.name))
+    }
+
+    @RequestMapping(value='/multipart' ,method = POST)
+    ResponseEntity<Map> multipart(@RequestParam('multipart')MultipartFile multipartFile) {
+        Map info = [
+          contentType: multipartFile.contentType,
+          name: multipartFile.name,
+          original: multipartFile.originalFilename
+        ]
+
+//    ResponseEntity.ok(info)
+        new ResponseEntity(info,HttpStatus.ACCEPTED)
     }
 }
